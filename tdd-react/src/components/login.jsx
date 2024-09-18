34f97081db1018a05904137f8b5d15ab34b1fe22
Abhinav Oruganti <orugantiabhinav5@gmail.com>
2024-09-18 08:51:47 +0530

    added tests to check the enable/disable to login button

--- tdd-react/src/components/login.jsx
+++ tdd-react/src/components/login.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <span data-testid="login-span-id">Login span id here</span>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeEmail = (event) => {
+    setEmail((prev) => event?.target?.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword((prev) => event?.target?.value);
+  };
+
+  const onLogin = () => {};
+
+  return (
+    <div>
+      <span data-testid="login-span-id">Login span id here</span>
+
+      <div className="row">
+        <label>Email</label>
+        <input data-testid="email-id" type="text" onChange={onChangeEmail} value={email} />
+      </div>
+
+      <div className="row">
+        <label>Password</label>
+        <input data-testid="password-id" type="password" onChange={onChangePassword} value={password}/>
+      </div>
+
+      <button disabled={!email || !password} onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
 
 export default Login;
--- tdd-react/unit-tests/login.spec.jsx
+++ tdd-react/unit-tests/login.spec.jsx
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom'
 import App from '../src/App';
 
 test("App component is loaded", () => {
-  render(<App/>);
+  renderApp();
   const spanElement = screen.getByTestId("app-span-id");
 
   expect(spanElement).toBeDefined();
@@ -13,7 +13,7 @@ test("App component is loaded", () => {
 });
 
 test("App component loaded and Login component is not loaded", () => {
-  render(<App/>);
+  renderApp();
   const spanElement = screen.getByTestId("app-span-id");
   const loginSpanElement = screen.queryByTestId("login-span-id");
 
@@ -22,12 +22,51 @@ test("App component loaded and Login component is not loaded", () => {
 });
 
 test("Login Component is loaded", () => {
-  render(<App />);
-
-  const loginButton = screen.getByText("Continue with Login");
-  fireEvent.click(loginButton);
+  triggerContinueLoginBtnClick();
 
   const loginSpanElement = screen.queryByTestId("login-span-id");
 
   expect(loginSpanElement).toHaveTextContent("Login span id here");
 });
+
+test("Login button disabled", () => {
+  triggerContinueLoginBtnClick();
+
+  const loginBtn = screen.getByText("Login");
+
+  expect(loginBtn.disabled).toBe(true)
+});
+
+test("Login button enabled", async () => {
+  triggerContinueLoginBtnClick();
+
+  const emailCtrl = screen.getByTestId("email-id");
+  const passwordCtrl = screen.getByTestId("password-id");
+
+  expect(emailCtrl.value).toBe("");
+  expect(passwordCtrl.value).toBe("");
+
+  const mockObj = { email: "test-email@email.com", password: "test-password-value" }
+  fireEvent.change(emailCtrl, {target: {value: mockObj.email}});
+  fireEvent.change(passwordCtrl, {target: {value: mockObj.password}});
+
+  expect(emailCtrl.value).toBe(mockObj.email);
+  expect(passwordCtrl.value).toBe(mockObj.password);
+
+  const loginBtn = screen.getByText("Login");
+
+  expect(loginBtn.disabled).toBe(false);
+});
+
+const triggerContinueLoginBtnClick = () => {
+  renderApp();
+
+  const continueloginButton = screen.getByText("Continue with Login");
+  fireEvent.click(continueloginButton);
+
+  
+}
+
+const renderApp = () => {
+  render(<App/>);
+}
